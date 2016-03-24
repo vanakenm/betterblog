f11dde26aa2f0899b2250ef30892f46af2f7c296
Martin Van Aken <martin@joyouscoding.com>
2016-03-24 10:39:16 +0100

    Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  root 'articles#index'
+    # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "News at eleven", body: "Chicken find in the wild")
+Article.create(title: "Weather", body: "Beautiful sky today")
